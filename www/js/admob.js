ce4e0ebb2ca4e5f75857ba3794e3031a1866fe75
tuebom <tuebom.wirya@gmail.com>
2016-08-02 17:13:22 +0800

    Update publisher ID

--- www/js/admob.js
+++ www/js/admob.js
@@ -1,18 +1,18 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+        banner: 'ca-app-pub-8007424208011196',
+        interstitial: 'ca-app-pub-8007424208011196'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+        banner: 'ca-app-pub-8007424208011196',
+        interstitial: 'ca-app-pub-8007424208011196'
     };
 } else {
     admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+        banner: 'ca-app-pub-8007424208011196',
+        interstitial: 'ca-app-pub-8007424208011196'
     };
 }
 
